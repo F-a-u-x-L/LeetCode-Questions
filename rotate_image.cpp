c7cfecfcf170fd4dd28e4e74ab27a5758fd550e5
unknown <fauxl.plays@gmail.com>
2022-08-24 19:07:26 +0530

    48 rotate image

--- rotate_image.cpp
+++ rotate_image.cpp
@@ -2,6 +2,10 @@
 #include<vector>
 using namespace std;
 
+// For 90deg    :   Transpose -> Swap columns
+// For 180deg   :   Swap columns -> Swap rows
+// For 270deg   :   Transpose -> Swap rows
+
 class Solution {
 
     public:
@@ -27,4 +31,31 @@ class Solution {
         }
 
     }
+};
+
+
+
+// Alternate method to transpose
+
+class Solution {
+
+    public:
+
+    void rotate(vector<vector<int>> &matrix) {
+
+        int n = matrix.size();
+
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<i; j++) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n/2; j++) {
+                swap(matrix[i][j], matrix[i][n-j-1]);
+            }
+        }
+    }
+
 };
