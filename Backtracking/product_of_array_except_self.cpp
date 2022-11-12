449b2134d519b65e14b0ec045b2852a2e3bc2b2b
FauxL <fauxl.plays@gmail.com>
2022-11-13 01:24:36 +0530

    238 product of array except self

---
+++ Backtracking/product_of_array_except_self.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+
+class Solution {
+
+    public:
+
+    vector<int> productExceptSelf(vector<int> &nums) {
+
+        int n = nums.size();
+        vector<int> ans;
+        vector<int> left(n);
+        vector<int>  right(n);
+
+        left[0] = 1;
+        for(int i=1; i<n; i++) {
+            left[i] = left[i-1] * nums[i-1];
+        }
+
+        right[n-1] = 1;
+        for(int i=n-2; i>=0; i--) {
+            right[i] = right[i+1] * nums[i+1];
+        }
+
+        for(int i=0; i<n; i++) {
+            ans.push_back(left[i] * right[i]);
+        }
+
+        return ans;
+    }
+};
+
+
+// Optimized Solution O(1) space
+
+class Solution {
+
+    public:
+
+    vector<int> productExceptSelf(vector<int> &nums) {
+
+        int n = nums.size();
+        vector<int> ans(n);
+
+        ans[n-1] = 1;
+        for(int i=n-2; i>=0; i--) {
+            ans[i] = ans[i+1] * nums[i+1];
+        }
+
+        int product = 1;
+        for(int i=0; i<n; i++) {
+            ans[i] = product * ans[i];
+            product = product * nums[i];
+        }
+
+        return ans;
+    }
+};
